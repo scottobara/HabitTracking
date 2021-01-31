1a6ea321920e98d0eb897a46f095f08d9230168b
Scott Obara <scott@rubbscreations.com>
2021-01-31 13:59:09 +1100

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Scott Obara on 31/1/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
